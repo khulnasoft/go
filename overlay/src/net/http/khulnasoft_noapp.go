0a4bc534448d3476e498bf4155be06f2830c8809
NxPKG <iconmamundentist@gmail.com>
2024-11-19 18:23:03 +0600

    Create khulnasoft_noapp.go
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

---
+++ overlay/src/net/http/khulnasoft_noapp.go
@@ -0,0 +1,14 @@
+//go:build !khulnasoft
+// +build !khulnasoft
+
+package http
+
+import (
+	"context"
+)
+
+// khulnasoftBeginRoundTrip is called by net/http when a RoundTrip begins.
+func khulnasoftBeginRoundTrip(req *Request) (context.Context, error) { return req.Context(), nil }
+
+// khulnasoftFinishRoundTrip is called by net/http when a RoundTrip completes.
+func khulnasoftFinishRoundTrip(req *Request, resp *Response, err error) {}
