7d1a6edf0ff52a7c4da0a7d6737492f59c89d290
NxPKG <iconmamundentist@gmail.com>
2024-11-19 18:29:34 +0600

    Create khulnasoft_noapp.go
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

---
+++ overlay/src/testing/khulnasoft_noapp.go
@@ -0,0 +1,28 @@
+//go:build !khulnasoft
+// +build !khulnasoft
+
+package testing
+
+// khulnasoftStartTest is called when a test starts running. This allows Khulnasoft's testing framework to
+// isolate behavior between different tests on global state.
+//
+// This implementation is simply a no-op as it's used when the tests are not being run against an Khulnasoft application
+func khulnasoftStartTest(t *T, fn func(t *T)) {}
+
+// khulnasoftEndTest is called when a test ends. This allows Khulnasoft's testing framework to clear down any state from the test
+// and to perform any assertions on that state that it needs to. It is linked to the Khulnasoft runtime via go:linkname.
+//
+// This implementation is simply a no-op as it's used when the tests are not being run against an Khulnasoft application
+func khulnasoftEndTest(t *T) {}
+
+// khulnasoftPauseTest is called when a test is paused. This allows Khulnasoft's testing framework to
+// isolate behavior between different tests on global state. It is linked to the Khulnasoft runtime via go:linkname.
+func khulnasoftPauseTest(t *T) {}
+
+// khulnasoftResumeTest is called when a test is resumed after being paused. This allows Khulnasoft's testing framework to clear down any state from the test
+// and to perform any assertions on that state that it needs to. It is linked to the Khulnasoft runtime via go:linkname.
+func khulnasoftResumeTest(t *T) {}
+
+// khulnasoftTestLog is called when a test logs a line. This allows Khulnasoft's testing framework to capture the log output
+// and emit that log output to the test trace.
+func khulnasoftTestLog(line string, frameSkip int) {}
